a4e79c9afe7c8471534af1a4bbc279886327e589
Utkarsh Kumar <utkarshkumar965@gmail.com>
2021-04-08 17:33:36 +0530

    Added Server

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const port = process.env.PORT || 8000;
+const {
+    connected
+} = require("process");
+const {
+    functions
+} = require("lodash");
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.urlencoded());
+app.use(methodOverride('_method'));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set("views", __dirname + "/views"); // set express to look in this folder to render our view
+
+
+
+app.get("/", function (req, res) {
+    res.render("index");
+});
+app.get("/CheckIn", function (req, res) {
+    res.render("CheckIn");
+});
+app.get("/CheckOut", function (req, res) {
+    res.render("CheckOut");
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
