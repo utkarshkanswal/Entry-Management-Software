25dc63a6dca757019150f4b586419d86cbb23844
Utkarsh Kumar <utkarshkumar965@gmail.com>
2021-04-15 17:59:57 +0530

    Added Mysql DataBase and added functionality on post request

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
+const mysql = require("mysql");
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'Employee',
+});
+db.connect(err => {
+    if (err)
+        console.log("Some Error Occurred");
+    else
+        console.log("You are Connected to Mysql Data Base");
+});
 const port = process.env.PORT || 8000;
 const {
     connected
@@ -31,14 +44,60 @@ app.get("/CheckIn", function (req, res) {
 app.get("/CheckOut", function (req, res) {
     res.render("CheckOut");
 });
-
+app.get('/createdatabase/:name', function (req, res) {
+    let database = req.params.name;
+    console.log(database);
+    let q = 'CREATE DATABASE ' + database;
+    console.log(q);
+    db.query(q, (err) => {
+        if (err)
+            res.send("Some Error Occurred");
+        else
+            res.send("Database Created");
+    });
+});
 app.post("/CheckIn", function (req, res) {
+    let obj = req.body;
+    let post = {
+        UserName: obj.Name[0],
+        UserEmail: obj.Email[0],
+        UserPhone: obj.PhoneNumber[0],
+        HostName: obj.Name[1],
+        HostEmail: obj.Email[1],
+        HostPhone: obj.PhoneNumber[1],
+    };
+    let q = 'INSERT INTO user SET ?'
+    db.query(q, post, (err) => {
+        if (err)
+            res.send("Some Error Occurred");
+        else {
+            res.send("Successfully Added");
+        }
+    });
     console.log(req.body);
-    res.send("Post Request");
 });
 app.post("/CheckOut", function (req, res) {
+    let obj = req.body;
     console.log(req.body);
-    res.send("Post Request");
+    let email = obj.Email;
+    let q = 'UPDATE user SET CheckOut=NOW() where UserEmail="' + email + '"';
+    console.log(q);
+    db.query(q, (err) => {
+        if (err)
+            res.send("Some Error Occurred");
+        else {
+            let q1 = 'SELECT UserName, HostEmail ,CheckIn, CheckOut from user where UserEmail="' + email + '"';
+            console.log(q1);
+            db.query(q1, (err, results) => {
+                if (err)
+                    res.send("Some Error Occurred");
+                else {
+                    console.log(results);
+                    res.send("Successfull");
+                }
+            });
+        }
+    });
 });
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
